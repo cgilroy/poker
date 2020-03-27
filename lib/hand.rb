6a34eeb5808ef5684a049bd8ce46d12be8e6ac8c
CJ Gilroy <c.gilroy9@gmail.com>
2020-03-27 13:42:33 -0600

    Add #swap_cards method

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,6 +1,15 @@
 
 class Hand
+    attr_reader :cards
     def initialize(cards)
         raise 'Hands must have 5 cards' if cards.count != 5
+        @cards = cards
+    end
+
+    def swap_cards(old_cards,new_cards)
+        raise 'Improper card counts' unless old_cards.length == new_cards.length && old_cards.length < 6
+        raise 'Cannot swap a card you do not have' unless old_cards.all? { |card| @cards.include?(card) }
+        new_cards.each { |card| @cards <<  card}
+        old_cards.each { |card| @cards.delete(card) }
     end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -20,4 +20,28 @@ describe Hand do
         end
     end
 
+    describe '#swap_cards' do
+        let(:cards_out) { hand.cards[0..1] }
+        let(:cards_in) { [Card.new(:heart,:three),Card.new(:spade,:king)] }
+
+        it 'removes the cards' do
+            hand.swap_cards(cards_out,cards_in)
+            expect(hand.cards).to_not include(*cards_out)
+        end
+
+        it 'includes the new cards' do
+            hand.swap_cards(cards_out,cards_in)
+            expect(hand.cards).to include(*cards_in)
+        end
+
+        it 'raises an error if swap does not end with 5 cards in the hand' do
+            expect {hand.swap_cards(cards_out,[Card.new(:spade,:six)])}.to raise_error 'Improper card counts'
+        end
+
+        it 'raises an error if you try to discard a card you do not own' do
+            expect do
+                hand.swap_cards([Card.new(:diamond,:ten)],cards_in[0..0])
+            end.to raise_error 'Cannot swap a card you do not have'
+        end
+    end
 end
