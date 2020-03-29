125ab63031d3d21a9d21ececfd36f1331c28abe3
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-03-29 10:20:56 -0600

    Setting up hand symbols and rank array

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -44,4 +44,137 @@ describe Hand do
             end.to raise_error 'Cannot swap a card you do not have'
         end
     end
+
+    describe 'poker hands' do
+        let(:royal_flush) do
+            Hand.new([
+                Card .new(:spade, :ace),
+                Card.new(:spade, :king),
+                Card.new(:spade, :queen),
+                Card.new(:spade, :jack),
+                Card.new(:spade, :ten)
+            ])
+        end
+
+        let(:straight_flush) do
+            Hand.new([
+                Card.new(:spade, :eight),
+                Card.new(:spade, :seven),
+                Card.new(:spade, :six),
+                Card.new(:spade, :five),
+                Card.new(:spade, :four)
+            ])
+        end
+
+        let(:four_of_a_kind) do
+            Hand.new([
+                Card.new(:spade, :ace),
+                Card.new(:heart, :ace),
+                Card.new(:diamond, :ace),
+                Card.new(:club, :ace),
+                Card.new(:spade, :ten)
+            ])
+        end
+
+        let(:full_house) do
+            Hand.new([
+                Card.new(:spade, :ace),
+                Card.new(:club, :ace),
+                Card.new(:spade, :king),
+                Card.new(:heart, :king),
+                Card.new(:diamond, :king)
+            ])
+        end
+
+        let(:flush) do
+            Hand.new([
+                Card.new(:spade, :four),
+                Card.new(:spade, :seven),
+                Card.new(:spade, :ace),
+                Card.new(:spade, :two),
+                Card.new(:spade, :eight)
+            ])
+        end
+
+        let(:straight) do
+            Hand.new([
+                Card.new(:heart, :king),
+                Card.new(:heart, :queen),
+                Card.new(:diamond, :jack),
+                Card.new(:club, :ten),
+                Card.new(:spade, :nine)
+            ])
+        end
+
+        let(:three_of_a_kind) do
+            Hand.new([
+                Card.new(:spade, :three),
+                Card.new(:diamond, :three),
+                Card.new(:heart, :three),
+                Card.new(:spade, :jack),
+                Card.new(:spade, :ten)
+            ])
+        end
+
+        let(:two_pair) do
+            Hand.new([
+                Card.new(:heart, :king),
+                Card.new(:diamond, :king),
+                Card.new(:spade, :queen),
+                Card.new(:club, :queen),
+                Card.new(:spade, :ten)
+            ])
+        end
+
+        let(:one_pair) do
+            Hand.new([
+                Card.new(:spade, :ace),
+                Card.new(:spade, :ace),
+                Card.new(:heart, :queen),
+                Card.new(:diamond, :jack),
+                Card.new(:heart, :ten)
+            ])
+        end
+
+        let(:high_card) do
+            Hand.new([
+                Card.new(:spade, :two),
+                Card.new(:heart, :four),
+                Card.new(:diamond, :six),
+                Card.new(:spade, :nine),
+                Card.new(:spade, :ten)
+            ])
+        end
+
+        let(:hand_ranks) do
+            [
+                :royal_flush,
+                :straight_flush,
+                :four_of_a_kind,
+                :full_house,
+                :flush,
+                :straight,
+                :three_of_a_kind,
+                :two_pair,
+                :one_pair,
+                :high_card
+            ]
+        end
+
+        let!(:hands) do
+            [
+                royal_flush,
+                straight_flush,
+                four_of_a_kind,
+                full_house,
+                flush,
+                straight,
+                three_of_a_kind,
+                two_pair,
+                one_pair,
+                high_card
+            ]
+        end
+
+    end
 end
